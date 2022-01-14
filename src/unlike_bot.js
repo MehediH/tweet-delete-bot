244089e376fd903f3eb2530a7ab7bf6effc9d30a
MehediH <mehedi.hassan@circle.com>
2022-01-14 15:24:11 +0000

    auto unlike

--- src/unlike_bot.js
+++ src/unlike_bot.js
@@ -3,12 +3,14 @@ const config = require("./config");
 const screen_name = process.env.SCREEN_NAME;
 
 const bot = new Twit(config);
-const jan13_2022 = 1642069781272;
+const jan13_2022 = 1641050207000;
+
+const filterTweets = ["1481378019562250240"];
 
 const getLikesAndDelete = async () => {
   const data = await bot.get("favorites/list", {
     screen_name: screen_name,
-    count: 1,
+    count: 200,
   });
 
   if (!data.data) return;
@@ -17,17 +19,36 @@ const getLikesAndDelete = async () => {
 
   // We get all likes after the given time, in this case jan13_2022
   const filteredLikes = likes.filter(
-    (t) => new Date(t.created_at).getTime() > jan13_2022
+    (t) =>
+      new Date(t.created_at).getTime() > jan13_2022 &&
+      !filterTweets.includes(t.id_str)
+  );
+
+  let i = 0;
+
+  console.log(
+    `Found ${filteredLikes.length} tweets at ${new Date(
+      Date.now()
+    ).toISOString()}`
   );
 
   const deleteLikes = filteredLikes.map(async (t) => {
-    await bot.post("favorites/destroy", {
-      id: t.id_str.toString(),
-    });
+    await bot
+      .post("favorites/destroy", {
+        id: t.id_str.toString(),
+      })
+      .catch((e) => {
+        console.log(`Couldn't delete tweet ${t.id_str.toString()}`, e.message);
+      })
+      .then(() => {
+        i += 1;
+      });
   });
 
   // delete and await
   await Promise.all(deleteLikes);
+
+  console.log(`Deleted ${i} tweets at ${new Date(Date.now()).toISOString()}`);
 };
 
 getLikesAndDelete();
